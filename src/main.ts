e908cb4d9de6226bc21184154ae7a8a481578958
Georgy <Aegoraa@yandex.ru>
2023-04-02 15:31:08 +0300

    refactor: moved pipes to common

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { setupSwagger } from './config/swagger.config';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
-import {ValidationPipe} from "@nestjs/common";
-import {TrimPipe} from "./pipes/trim.pipe";
+import { useGlobalPipes } from './common/pipes/global.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
-  app.useGlobalPipes(
-      new TrimPipe(),
-      new ValidationPipe({
-        whitelist: true,
-        transform: true,
-        stopAtFirstError: true,
-      }),
-  );
+  useGlobalPipes(app);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(4000);
 }
