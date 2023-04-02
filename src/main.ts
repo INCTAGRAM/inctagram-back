b02999bb530d75d05a7ca8d5e552fd4749c4dfce
Georgy <Aegoraa@yandex.ru>
2023-04-02 15:34:39 +0300

    refactor: moved global filter to common

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { setupSwagger } from './config/swagger.config';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { useGlobalPipes } from './common/pipes/global.pipe';
+import {useGlobalFilters} from "./common/filters/global.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
   useGlobalPipes(app);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  useGlobalFilters(app);
   await app.listen(4000);
 }
 bootstrap();
