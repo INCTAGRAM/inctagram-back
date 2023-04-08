2967481ba01fc1fe382a8ad579334772270dd28a
Georgy <Aegoraa@yandex.ru>
2023-04-08 17:24:51 +0300

    refactor: fixed bug with logout

--- src/adaptors/adaptor.module.ts
+++ src/adaptors/adaptor.module.ts
@@ -3,9 +3,10 @@ import { JwtAdaptor } from './jwt/jwt.adaptor';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { BcryptAdaptor } from './bcrypt/bcrypt.adaptor';
+import { DeviceSessionsModule } from '../deviceSessions/device-sessions.module';
 
 @Module({
-  imports: [UserModule, JwtModule.register({})],
+  imports: [UserModule, DeviceSessionsModule, JwtModule.register({})],
   controllers: [],
   providers: [JwtAdaptor, BcryptAdaptor],
   exports: [JwtAdaptor, BcryptAdaptor],
