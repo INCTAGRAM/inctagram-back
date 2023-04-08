7ae8b6cafcd07df23f871b8ce8dcca3a4458ce25
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-08 21:27:40 +0300

    chore: add consants file

---
+++ src/common/constants.ts
@@ -0,0 +1,12 @@
+export const API = {
+  USERS: 'api/users',
+};
+
+// Field of incomfing HTTP message with file data
+export const FILE_FIELD = 'file';
+
+export const MAX_AVATAR_SIZE = 1024 * 1024 * 2;
+export const MIN_AVATAR_HEIGHT = 400;
+export const MIN_AVATAR_WIDTH = 400;
+export const MAX_AVATAR_HEIGHT = 800;
+export const MAX_AVATAR_WIDTH = 1200;
