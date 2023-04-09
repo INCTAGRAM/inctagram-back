76c2da79c912af1dcc24fd77ef5cdbe2b8245be9
Georgy <Aegoraa@yandex.ru>
2023-04-09 13:35:48 +0300

    feat:added cqrsmodule into device-sessions module

--- src/deviceSessions/device-sessions.module.ts
+++ src/deviceSessions/device-sessions.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeviceSessionsRepository } from './repositories/device-sessions.repository';
 import { DeviceSessionsController } from './api/device-sessions.controller';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [],
+  imports: [CqrsModule],
   controllers: [DeviceSessionsController],
   providers: [DeviceSessionsRepository],
   exports: [DeviceSessionsRepository],
