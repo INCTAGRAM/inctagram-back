833bf2b2b4f57470a7cd041b2c92cbde040a985c
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-09 23:53:34 +0300

    feat: add validation schema for configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,15 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AdaptorModule } from './adaptors/adaptor.module';
 import { DeviceSessionsModule } from './deviceSessions/device-sessions.module';
+import { config } from './config/configuration';
+import { configValidationSchema } from './config/validation-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: configValidationSchema,
     }),
     AuthModule,
     UserModule,
