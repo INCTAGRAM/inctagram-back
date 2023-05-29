c06549603533b5bc99a018a3d9ae8d55b3677b10
Aleksandr Schemelev <prophreaker@gmail.com>
2023-05-29 14:28:45 +0300

    feat: add id field to PostsQueryDto

--- src/user/dto/posts-query.dto.ts
+++ src/user/dto/posts-query.dto.ts
@@ -1,5 +1,7 @@
+import { IsNumber, IsOptional, IsUUID } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { IsNumber } from 'class-validator';
+
+import { ValidateIfOtherNotExists } from 'src/common/decorators/validate-if-other-not-exists.decorator';
 
 export class PostsQueryDto {
   @IsNumber()
@@ -8,6 +10,8 @@ export class PostsQueryDto {
 
     return parsedValue > 0 ? parsedValue : 1;
   })
+  @ValidateIfOtherNotExists('id')
+  @IsOptional()
   public page: number;
 
   @IsNumber()
@@ -17,4 +21,8 @@ export class PostsQueryDto {
     return parsedValue > 0 ? parsedValue : 9;
   })
   public pageSize: number;
+
+  @IsUUID()
+  @IsOptional()
+  public id: string;
 }
